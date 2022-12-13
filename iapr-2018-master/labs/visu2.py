b16136a7373acc9c2a7dc3a8daac9b229a9a4930
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 18:09:58 +0000

    Adding tarfile member sanitization to extractall()

--- iapr-2018-master/labs/visu2.py
+++ iapr-2018-master/labs/visu2.py
@@ -5,7 +5,26 @@ data_base_path = os.path.join(os.pardir, 'data')
 data_folder = 'lab-02-data'
 tar_path = os.path.join(data_base_path, data_folder + '.tar.gz')
 with tarfile.open(tar_path, mode='r:gz') as tar:
-    tar.extractall(path=data_base_path)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar, path=data_base_path)
 
 import skimage.io
 from skimage import measure
